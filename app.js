e0ea0bd4267ce9a6d40a1c6b92d9ae5de4750ff1
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-12 22:03:20 +0300

    Add innerHTML functions to populate the data styled with css

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 const poke_container = document.getElementById('poke_container');
 const pokemon_number = 150;
+
+const fetchPokemon = async () => {
+	for (let i = 1; i <= pokemon_number; i++) {
+		await getPokemon(i);
+	}
+};
+
+const getPokemon = async (id) => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const result = await fetch(url);
+	const pokemon = await result.json();
+	console.log(pokemon);
+	createPokemonCard(pokemon);
+};
+
+fetchPokemon();
+
+function createPokemonCard(pokemon) {
+	const pokemonElement = document.createElement('div');
+	pokemonElement.classList.add('pokemon');
+
+	const pokeInnerHTML = `
+		${pokemon.name};
+	`;
+
+	pokemonElement.innerHTML = pokeInnerHTML;
+
+	poke_container.appendChild(pokemonElement);
+}
